5fcf48d68a9ab558b6a3a9f5f2b24d9eb4c17f48
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-26 11:40:30 +0530

    Commit

---
+++ src/main/java/com/example/SpringSecurityP01/config/webConfig.java
@@ -0,0 +1,25 @@
+package com.example.SpringSecurityP01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class webConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+
+        httpSecurity.authorizeHttpRequests(req->
+                req.requestMatchers("/updates","/contact").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
